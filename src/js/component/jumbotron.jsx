a16438434fb540c578fd69ece7953ab2d1d3dda2
nehemias10 <nehemiasrivero1@gmail.com>
2022-01-09 15:56:37 +0000

    uno

---
+++ src/js/component/jumbotron.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Jumbotron = () => (
+	<div className="jumbotron">
+		<h1 className="display-4">A Warm Welcome!</h1>
+		<p className="lead">
+			Contrary to popular belief, Lorem Ipsum is not simply random text.
+			It has roots in a piece of classical Latin literature from 45 BC,
+			making it over 2000 years old.
+		</p>
+		<a className="btn btn-primary btn-lg" href="#" role="button">
+			Call to action!
+		</a>
+	</div>
+);
