8f00abc59627ef215d41e5fed4003611549759bd
nehemias10 <nehemiasrivero1@gmail.com>
2022-06-16 20:09:54 +0000

    2021

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -9,7 +9,7 @@ export const Footer = () => {
 		<footer className="py-5 bg-dark footer" style={styleFooter}>
 			<div className="container">
 				<p className="m-0 text-center text-white">
-					Copyright © Your Website 2019
+					Copyright © Your Website 2021
 				</p>
 			</div>
 		</footer>
